c8c598efc41c871d8db866ab5e1663cd65aeca85
zw lin <zhiwen@kooboo.cn>
2019-06-11 23:56:43 +0800

    revert

---
+++ src/deploy/server.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var serveStatic = require('serve-static')
+var helmet = require('helmet')
+var app = express()
+
+// app.disable('x-powered-by')
+
+app.use(
+  helmet({
+    frameguard: {
+      action: 'SAMEORIGIN'
+    }
+  })
+)
+
+app.use(
+  helmet.hsts({
+    maxAge: 31536000,
+    includeSubDomains: true
+  })
+)
+
+app.use(require('connect-history-api-fallback')())
+
+app.use(serveStatic('./dist'))
+
+var port = 8090
+app.listen(port)
+
+console.log('server started ' + port)
